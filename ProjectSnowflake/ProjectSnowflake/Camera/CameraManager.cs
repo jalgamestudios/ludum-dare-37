5147403bee37e7629cfaf5f3fd0784cca9b2edbd
Johannes Lehmann <jal@jalfm.de>
2016-12-10 16:32:56 +0100

    Improved camera-following

--- ProjectSnowflake/ProjectSnowflake/Camera/CameraManager.cs
+++ ProjectSnowflake/ProjectSnowflake/Camera/CameraManager.cs
@@ -26,6 +26,26 @@ namespace ProjectSnowflake.Camera
             return new Rectangle((int)(offsetPosition.X * 16), (int)(offsetPosition.Y * 16), (int)(size.X * 16), (int)(size.Y * 16));
         }
 
+        public static Vector2 getWorldPosition(int x, int y)
+        {
+            return new Vector2(x, y) / 16 + offset;
+        }
+
+        public static Vector2 getWorldPosition(Rectangle screenPosition)
+        {
+            return new Vector2(screenPosition.X, screenPosition.Y) / 16 + offset;
+        }
+
+        public static Vector2 getWorldSize(int x, int y)
+        {
+            return new Vector2(x, y) / 16;
+        }
+
+        public static Vector2 getWorldSize(Rectangle screenPosition)
+        {
+            return new Vector2(screenPosition.X, screenPosition.Y) / 16;
+        }
+
         #endregion
     }
 }
--- ProjectSnowflake/ProjectSnowflake/Entities/Components/CameraTrackingComponent.cs
+++ ProjectSnowflake/ProjectSnowflake/Entities/Components/CameraTrackingComponent.cs
@@ -1,4 +1,6 @@
-﻿using ProjectSnowflake.Camera;
+﻿using Microsoft.Xna.Framework;
+using ProjectSnowflake.Camera;
+using ProjectSnowflake.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +13,19 @@ namespace ProjectSnowflake.Entities.Components
     {
         #region Variables
 
-
+        public int horizontalBorder { get; set; }
+        public int verticalBorder { get; set; }
 
         #endregion
 
 
         #region Constructors
+
+        public CameraTrackingComponent()
+        {
+            this.horizontalBorder = (int)(RenderingManager.screenDimensions.X * 0.35f);
+            this.verticalBorder = (int)(RenderingManager.screenDimensions.Y * 0.35f);
+        }
 
         #endregion
 
@@ -25,8 +34,27 @@ namespace ProjectSnowflake.Entities.Components
 
         public void update(Entity entity)
         {
-            CameraManager.offset = entity.position - CameraManager.worldFrameSize / 2;
+            Vector2 offset = CameraManager.offset;
 
+            Rectangle ps = CameraManager.getScreenPosition(entity.position + entity.colliderSize / 2, new Vector2());
+
+            if (ps.X < horizontalBorder)
+                offset.X = (entity.position + entity.colliderSize / 2).X
+                    - CameraManager.getWorldSize(horizontalBorder, 0).X;
+
+            if (ps.X > RenderingManager.screenDimensions.X - horizontalBorder)
+                offset.X = (entity.position + entity.colliderSize / 2).X
+                    - CameraManager.getWorldSize(RenderingManager.screenDimensions.X - horizontalBorder, 0).X - (1 / 16f);
+
+            if (ps.Y < verticalBorder)
+                offset.Y = (entity.position + entity.colliderSize / 2).Y
+                    - CameraManager.getWorldSize(0, verticalBorder).Y;
+
+            if (ps.Y > RenderingManager.screenDimensions.Y - verticalBorder)
+                offset.Y = (entity.position + entity.colliderSize / 2).Y
+                    - CameraManager.getWorldSize(0, RenderingManager.screenDimensions.Y - verticalBorder).Y - (1 / 16f);
+
+            CameraManager.offset = offset;
         }
 
         public void draw(Entity entity)
