072779aa0e0209924cddb13ce346a45767412545
Johannes Lehmann <jal@jalfm.de>
2016-12-11 22:44:16 +0100

    Added smooth turning

--- ProjectSnowflake/ProjectSnowflake/Entities/Components/WalkingTextureComponent.cs
+++ ProjectSnowflake/ProjectSnowflake/Entities/Components/WalkingTextureComponent.cs
@@ -19,6 +19,8 @@ namespace ProjectSnowflake.Entities.Components
 
         public float rotation { get; set; }
 
+        public float targetRotation { get; set; }
+
         #endregion
 
 
@@ -42,9 +44,52 @@ namespace ProjectSnowflake.Entities.Components
         {
             Vector2 center = entity.position + entity.colliderSize / 2;
             Rectangle position = CameraManager.getScreenPosition(center, new Vector2(1, 1));
+            float targetRotation = rotation;
             if (entity.direction.Length() > 0)
-                rotation = (float)Math.Atan2(entity.actualDirection.Y, entity.actualDirection.X) + MathHelper.PiOver2;
+                targetRotation = (float)Math.Atan2(entity.actualDirection.Y, entity.actualDirection.X) + MathHelper.PiOver2;
+
+            rotation = rotationStep(rotation, targetRotation, 6, Time.gameElapsedSeconds);
+
             RenderingManager.spriteBatch.Draw(texture, position, null, Color.White, rotation, new Vector2(8,8), SpriteEffects.None, 0);
+        }
+
+
+        private float rotationStep(float currentRotation, float wantedRotation, float speed, float time)
+        {
+            if (wantedRotation > MathHelper.Pi)
+                wantedRotation -= MathHelper.TwoPi;
+            if (wantedRotation <= -MathHelper.Pi)
+                wantedRotation += MathHelper.TwoPi;
+
+            if (currentRotation < wantedRotation && Math.Abs(wantedRotation - currentRotation) < MathHelper.Pi)
+            {
+                currentRotation += speed * time;
+                if (currentRotation > wantedRotation)
+                    currentRotation = wantedRotation;
+            }
+            else if (currentRotation < wantedRotation && Math.Abs(wantedRotation - currentRotation) >= MathHelper.Pi)
+            {
+                currentRotation -= speed * time;
+                if (currentRotation > wantedRotation)
+                    currentRotation = wantedRotation;
+            }
+            if (currentRotation > wantedRotation && Math.Abs(currentRotation - wantedRotation) < MathHelper.Pi)
+            {
+                currentRotation -= speed * time;
+                if (currentRotation < wantedRotation)
+                    currentRotation = wantedRotation;
+            }
+            else if (currentRotation > wantedRotation && Math.Abs(currentRotation - wantedRotation) >= MathHelper.Pi)
+            {
+                currentRotation += speed * time;
+                if (currentRotation < wantedRotation)
+                    currentRotation = wantedRotation;
+            }
+            if (currentRotation > MathHelper.Pi)
+                currentRotation -= MathHelper.TwoPi;
+            if (currentRotation <= -MathHelper.Pi)
+                currentRotation += MathHelper.TwoPi;
+            return currentRotation;
         }
 
         #endregion
